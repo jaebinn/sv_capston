9bb981362615f8dac79173dac4f2a45c9daaf5f2
shin <whiteclover129@gmail.com>
2023-09-11 18:18:58 +0900

    second commit

---
+++ register.js
@@ -0,0 +1,6 @@
+const register_submit = document.querySelector("#register_submit");
+const user_name = document.querySelector("#user_name");
+
+register_submit.addEventListener("click", function () {
+  alert(`${user_name} ¹Ý°©½À´Ï´Ù.`);
+});
