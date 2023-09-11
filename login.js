d4c1c8acb0e8eed5c421418241d939831663320e
shin <whiteclover129@gmail.com>
2023-09-11 16:16:14 +0900

    second commit

---
+++ login.js
@@ -0,0 +1,6 @@
+let inputBox = document.querySelectorAll(".input-box");
+
+const inputId = document.getElementById("inputId"),
+  inputPw = document.getElementById("inputPw"),
+  loginBtn = document.getElementById("loginBtn");
+const idLength = inputId.value.length;
