5e8865ae66b53f89925854c3186bd1810219d219
shin <whiteclover129@gmail.com>
2023-11-20 13:36:37 +0900

    final

--- introduce.js
+++ introduce.js
@@ -1,4 +1,7 @@
 const tabList = document.querySelectorAll(".tab_menu .list li");
+const radio1 = document.getElementById("#radio1");
+const radio2 = document.getElementById("#radio2");
+const radio3 = document.getElementById("#radio3");
 for (var i = 0; i < tabList.length; i++) {
   tabList[i].querySelector(".btn").addEventListener("click", function (e) {
     e.preventDefault();
@@ -8,3 +11,13 @@ for (var i = 0; i < tabList.length; i++) {
     this.parentNode.classList.add("is_on");
   });
 }
+
+radio1.addEventListener("change", function () {
+  console.log("Option 1 selected");
+});
+radio2.addEventListener("change", function () {
+  console.log("Option 2 selected");
+});
+radio3.addEventListener("change", function () {
+  console.log("Option 3 selected");
+});
